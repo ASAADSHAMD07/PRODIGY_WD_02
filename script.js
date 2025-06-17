c87f1dd813c5541b2323fd1aa684902a1b9db22b
ASAADSHAMD07 <shaikhnisar99000@gmail.com>
2025-06-17 18:39:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-// script.js
-
-window.addEventListener("scroll", function () {
-  const navbar = document.getElementById("navbar");
-  if (window.scrollY > 50) {
-    navbar.classList.add("scrolled");
-  } else {
-    navbar.classList.remove("scrolled");
-  }
-});
+function getStarted() {
+  alert("Welcome! Let's get started with Asad Ahmad's website.");
+}
